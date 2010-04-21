426d97bd236b903f591e970589c64c5634781aa5
Evan Winslow <evan.b.winslow@gmail.com>
2010-04-21 20:51:45 +0000

    added ElggUser friending functions (non-functional)

--- js/users.js
+++ js/users.js
@@ -6,3 +6,31 @@ elgg.ElggUser = function(o) {
 };
 
 elgg.inherit(elgg.ElggUser, elgg.ElggEntity);
+
+/**
+ * Add the specified user as a friend of this user
+ *
+ * @param {Number} guid 
+ */
+elgg.ElggUser.prototype.addFriend = function(guid) {
+ 	elgg.api('friend.add', {
+ 		type: 'post',
+ 		data: {
+ 			guid: guid
+ 		}
+ 	});
+};
+
+/**
+ * Remove the specified user as a friend of this user
+ *
+ * @param {Number} guid 
+ */
+elgg.ElggUser.prototype.removeFriend = function(guid) {
+ 	elgg.api('friend.remove', {
+ 		type: 'post',
+ 		data: {
+ 			guid: guid
+ 		}
+ 	});
+};
