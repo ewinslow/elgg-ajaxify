9ddc254ae3306f60689a83364ad9935474e60494
Evan Winslow <evan.b.winslow@gmail.com>
2010-04-21 20:51:22 +0000

    Added group join/leave functions

--- js/groups.js
+++ js/groups.js
@@ -1,8 +1,39 @@
 /**
- * ElggGroup support
+ * ElggGroup
+ * 
+ * @param {Object} o
+ * @base ElggEntity
+ * @constructor
  */
 elgg.ElggGroup = function(o) {
-	elgg.ElggEntity.call(this, o);
+	elgg.ElggEntity.call(this,o);
 };
 
 elgg.inherit(elgg.ElggGroup, elgg.ElggEntity);
+
+
+/**
+ * Join the logged in user to a group
+ */
+elgg.ElggGroup.prototype.join = function() {
+	var _this = this;
+ 	elgg.api('group.join', {
+ 		type: 'post',
+ 		data: {
+	 		guid: _this.guid
+ 		}
+ 	});
+};
+
+/**
+ * Make the logged in user leave the group
+ */
+elgg.ElggGroup.prototype.leave = function() {
+	var _this = this;
+ 	elgg.api('group.leave', {
+ 		type: 'post',
+ 		data: {
+	 		guid: _this.guid
+ 		}
+ 	});
+};
