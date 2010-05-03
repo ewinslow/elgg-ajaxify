a82a2ea2881c018e91592d1d00c3936cc74bd90e
Evan Winslow <evan.b.winslow@gmail.com>
2010-05-03 23:12:07 +0000

    added slashes to json encoding before parsing with $.parseJSON

--- views/default/js/languages.php
+++ views/default/js/languages.php
@@ -5,7 +5,7 @@
 global $CONFIG;
 
 $language = $vars['language'];
-$json = json_encode($CONFIG->translations[$language]);
+$json = addslashes(json_encode($CONFIG->translations[$language]));
 
 header("Content-Type: application/x-javascript");
 echo "elgg.config.translations.$language = $.parseJSON('$json');";
