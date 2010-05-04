1d907a9c8da3a83656ffd4698d27464892d488d7
Evan Winslow <evan.b.winslow@gmail.com>
2010-05-04 03:21:13 +0000

    Ajax plugin enable/disable

--- js/ui.plugins.js
+++ js/ui.plugins.js
@@ -5,8 +5,35 @@ $(function() {
 });
 
 elgg.ui.plugins.init = function() {
-	$('a.pluginsettings_link').click(elgg.ui.plugins.toggleSettings);
-	$('a.manifest_details').click(elgg.ui.plugins.toggleDetails);
+//	$('a.pluginsettings_link').click(elgg.ui.plugins.toggleSettings);
+//	$('a.manifest_details').click(elgg.ui.plugins.toggleDetails);
+	
+	$('.admin_plugin_enable_disable a').click(function() {
+		var $plugin = $(this).closest(".plugin_details").toggleClass('active').toggleClass('not-active');
+		elgg.post(this.href, {
+			dataType: 'json',
+			success: function(data) {
+				if (data.status) {
+					elgg.register_error(data.system_messages.errors[0]);
+				} else {
+					//swallow system messages
+				}
+
+			}
+		});
+		
+		if($plugin.hasClass('active')) {
+			$(this).text(elgg.echo('disable'));
+			this.href = this.href.replace(/enable/, 'disable');
+		} else if($plugin.hasClass('not-active')) {
+			$(this).text(elgg.echo('enable'));
+			this.href = this.href.replace(/disable/, 'enable');
+		}
+		
+		return false;
+	});
+	
+
 };
 
 // toggle plugin's settings and more info on admin tools admin
