cf7990ab71de99373a1a281f375a8d0279ade64e
Ravindra Nath Kakarla <ravindhranath@gmail.com>
2011-06-27 03:04:30 +0530

    experimenting livesearch

--- start.php
+++ start.php
@@ -9,6 +9,9 @@ function ajaxify_init() {
 
 	$cached_url = elgg_get_simplecache_url('js', 'comments_ajaxify');
 	elgg_register_js('elgg.ajaxify.comments', $cached_url, 'footer');
+
+	$cached_url = elgg_get_simplecache_url('js', 'search_ajaxify');
+	elgg_register_js('elgg.ajaxify.search', $cached_url, 'footer');
 	
 	elgg_register_js('jquery.livequery', 'mod/elgg-ajaxify/vendors/livequery/jquery.livequery.js', 'footer');
 	
@@ -24,6 +27,7 @@ function ajaxify_init() {
 	elgg_load_js('elgg.ajaxify');
 	elgg_load_js('elgg.ajaxify.likes');
 	elgg_load_js('elgg.ajaxify.comments');
+	elgg_load_js('elgg.ajaxify.search');
 }
 
 elgg_register_event_handler('init', 'system', 'ajaxify_init');
