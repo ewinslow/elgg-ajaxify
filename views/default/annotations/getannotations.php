3729019c253125dd74d3132d37cdc8c20959b935
Ravindra Nath Kakarla <ravindhranath@gmail.com>
2011-06-30 01:35:53 +0530

    Viewing river comments works good

--- views/default/annotations/getannotations.php
+++ views/default/annotations/getannotations.php
@@ -4,13 +4,16 @@ $annotation_id = (int) get_input('annotation_id');
 $limit = (int) get_input('limit');
 $annotation_name = get_input('annotation_name');
 $guid = (int) get_input('guid');
+$order = get_input('order', 'desc');
+$offset = (int) get_input('offset', 0);
 
 $options = array(
 	'limit' => $limit,
 	'guid' => $guid,
 	'annotation_name' => $annotation_name,
 	'annotation_id' => $annotation_id,
-	'order_by' => 'n_table.time_created desc'
+	'order_by' => "n_table.time_created $order",
+	'offset' => $offset,
 );
 
 echo elgg_list_annotations($options);
