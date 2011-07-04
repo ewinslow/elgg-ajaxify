501f5189ad6e51a0226ea71b6a320f3880b42373
Ravindra Nath Kakarla <ravindhranath@gmail.com>
2011-07-05 02:46:11 +0530

    Navigating through pages works perfect - #17

--- views/default/entities/getentity.php
+++ views/default/entities/getentity.php
@@ -6,15 +6,31 @@
 //Incomplete
 if (elgg_is_xhr()) {
 	$subtype = get_input('subtype');
-	$guid =  (int) get_input('guid');
-	$limit =  (int) get_input('limit');
-	$entities = elgg_list_entities(array(
-		'type' => 'object',
-		'subtype' => $subtype,
-		'limit' => $limit,
-		'guid' => $guid,
-		'pagination' => FALSE
-	));
+	$guid = get_input('guid');
+	$limit =  get_input('limit');
+	$pagination = (boolean) get_input('pagination', FALSE);
+	$offset = get_input('offset');
+	$page_type = get_input('page_type');
+
+	switch ($page_type) {
+		case 'friends': 
+			$entities = list_user_friends_objects(elgg_get_logged_in_user_guid(), $subtype, $limit);
+			break;
+		case 'owner':
+			$owner_guid = elgg_get_logged_in_user_guid();
+		default:
+			$options = array(
+				'type' => 'object',
+				'subtype' => $subtype,
+				'limit' => $limit,
+				'guid' => $guid,
+				'owner_guid' => $owner_guid,
+				'pagination' => $pagination,
+				'offset' => $offset,
+			);
+			$entities = elgg_list_entities($options);
+			break;
+	}
 	echo $entities;
 }
 ?>
--- views/default/js/pagination_ajaxify.php
+++ views/default/js/pagination_ajaxify.php
@@ -2,7 +2,9 @@ elgg.provide('elgg.ajaxify.pagination');
 
 elgg.ajaxify.pagination.init = function() {
 	//Get context from the viewname (activity, thewire, blog etc.)
+	//(Why?) Activity river stream and entity lists use different DOMs.
 	elgg.ajaxify.pagination.context = elgg.ajaxify.getViewFromURL('activity').split('/')[0];
+
 	$('.elgg-pagination li').live('click', function(event) {
 		elgg.trigger_hook('read:submit', 'pagination', {'type': elgg.ajaxify.pagination.context}, {
 			'item': $(this),
@@ -15,12 +17,12 @@ elgg.ajaxify.pagination.init = function() {
 };
 
 elgg.ajaxify.pagination.read_submit = function(hook, type, params, value) {
-	if (params.type === 'activity') {
-		$('.elgg-pagination').before(elgg.ajaxify.ajaxLoader);
-	}
+	$('.elgg-pagination').before(elgg.ajaxify.ajaxLoader);
 };
 
 elgg.ajaxify.pagination.read_success = function(hook, type, params, value) {
+	//Activity river can be filtered using elgg-river-selector, the filter selected has to be preserved when a next page is requested, 
+	//sending type, subtype, page_type helps in getting the exact content under the applied filters
 	if (params.type === 'activity') {
 		elgg.view('river/getactivity', {
 			cache: false,
@@ -28,10 +30,34 @@ elgg.ajaxify.pagination.read_success = function(hook, type, params, value) {
 				type: $.url().param('type') || 'all',
 				subtype: $.url().param('subtype') || '',
 				page_type: elgg.ajaxify.getViewFromURL('').split('/')[1] || '',
+				offset: $(value.item).find('a').url().param('offset') || '', 
+			},
+			success: function(response) {
+				var newRiver = $(response)[0];
+				var newPagination = $(response)[1];
+				$('.elgg-river').replaceWith(newRiver);
+				$('.elgg-pagination').replaceWith(newPagination);
+				$(elgg.ajaxify.ajaxLoader).remove();
+				$('body').animate({scrollTop: 0}, 'slow');
+			},
+		});
+	} else {
+		elgg.view('entities/getentity', {
+			cache: false,
+			data: {
+				subtype: elgg.ajaxify.pagination.context,
+				limit: 15,
+				page_type: elgg.ajaxify.getViewFromURL('').split('/')[1] || '',
+				pagination: 'TRUE',
 				offset: $(value.item).find('a').url().param('offset') || '',
 			},
 			success: function(response) {
-				
+				var newList = $(response)[0];
+				var newPagination = $(response)[1];
+				$('.elgg-entity-list').replaceWith(newList);
+				$('.elgg-pagination').replaceWith(newPagination);
+				$(elgg.ajaxify.ajaxLoader).remove();
+				$('body').animate({scrollTop: 0}, 'slow');
 			},
 		});
 	}
