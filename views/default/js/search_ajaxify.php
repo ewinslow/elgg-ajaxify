93a205d7bbfeb53bf5d9472fda2ad3deada33e0e
Ravindra Nath Kakarla <ravindhranath@gmail.com>
2011-07-08 03:03:42 +0530

    Works well -- need to test more -- #44

--- views/default/js/search_ajaxify.php
+++ views/default/js/search_ajaxify.php
@@ -3,30 +3,75 @@ elgg.provide('elgg.ajaxify.search');
 elgg.ajaxify.search.init = function() {
 	elgg.ajaxify.search.fetch_limit = 10;
 	elgg.ajaxify.search.context = elgg.ajaxify.getViewFromURL('all').split('/')[0];
+	elgg.ajaxify.search.strip_desc = 40;
 
-//Incomplete
 	$('#elgg-search-autocomplete').autocomplete({
+		minLength: 2,
+		html: true,
+		max: elgg.ajaxify.search.fetch_limit,
 		source: function(req, res) {
 			elgg.get('livesearch', {
 				cache: false,
 				data: {
-					'q': req.term,
+					'term': req.term,
 					'match_on': elgg.ajaxify.search.context,
-					'limit': elgg.ajaxify.search.fetch_limit,
 				},
 				dataType: 'json',
 				success: function(response) {
 					res($.map(response, function(result) {
 						return {
-							label: result.icon+" "+result.desc,
+							//Todo trigger a handler to return subtype specific label
+							label: result.icon+" <a>"+result.name+"</a><br />"+result.desc.substr(0, elgg.ajaxify.search.strip_desc)+"...",
+							attributes: result,
 						}
 					}));
 				},
 			});
 		},
-		minLength: 2,
-		html: true,
+		select: function(event, ui) {
+			elgg.trigger_hook('read:submit', 'search', {'type': elgg.ajaxify.search.context}, {
+				ui: ui,
+			});
+			return false;
+		},
 	});
-}
+};
 
+elgg.ajaxify.search.read_submit = function(hook, type, params, value) {
+	var forward_url = '';
+	var guid = value.ui.item.attributes.guid;
+	var name = value.ui.item.attributes.name;
+	var desc = value.ui.item.attributes.desc;
+	var entity_type = value.ui.item.attributes.type;
+	switch (params.type) {
+		case 'blog':
+		case 'pages':
+		case 'file':
+		case 'bookmarks':
+			forward_url = params.type+'/view/'+guid;
+			break;
+		case 'thewire':
+			forward_url = params.type+'/thread/'+guid;
+			break;
+		case 'members': 
+			forward_url = '/profile/'+desc;
+			break;
+		case 'groups':
+			forward_url = params.type+'/profile/'+guid;
+			break;
+		case 'all':
+			switch (entity_type) {
+				case 'user': 
+					forward_url = '/profile/'+desc;
+					break;
+				case 'group':
+					forward_url = 'groups/profile/'+guid;
+					break;
+			}	
+			break;
+	}
+	elgg.forward(elgg.normalize_url(forward_url));
+};
+
+elgg.register_hook_handler('read:submit', 'search', elgg.ajaxify.search.read_submit); 
 elgg.register_hook_handler('init', 'system', elgg.ajaxify.search.init);
